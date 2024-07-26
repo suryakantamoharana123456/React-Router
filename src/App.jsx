5744affe95d9ae5d37a044a657cf9abc7736a46f
suryakantamoharana123456 <suryakantamoharana60019@gmail.com>
2024-07-27 01:03:03 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+ 
+import React from "react";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Contact from "./components/Contact/Contact";
+import About from "./components/About/About";
+
+ 
+ function App() {
+  return (
+    <>
+    {/* <Header/>
+    <Home/>
+    <About/>
+    <Contact/>
+    <Footer/> */}
+    </>
+  )
+}
+export default App;
